2d1c269fd8212e7d0c5b11b2b7598e4c090eabb0
Emmanuel <emmanuelmaldonado2101@gmail.com>
2025-04-06 23:09:09 -0600

    Se realizó el código para el funcionamiento de la aplicacion en streamlit

--- app.py
+++ app.py
@@ -2,15 +2,37 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
         
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
-        
-if hist_button: # al hacer clic en el botón
-    # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-        
-    # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
-        
-    # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+vehicles = pd.read_csv('vehicles_us.csv') # leer los datos
+
+st.header('Analisis de un dataframe sobre coches')
+if st.checkbox('Construir histograma para visualizar la distribución de precios'):
+    fig = px.histogram(vehicles, x='price', nbins=50, title='Distribución de Precios')
+    fig.show()
+
+if st.checkbox("Construir un gráfico de barras de la cantidad de vehículos por año de modelo"):
+    model_year_counts = vehicles['model_year'].value_counts().reset_index()
+    fig = px.bar(model_year_counts, x=model_year_counts.columns[0], y=model_year_counts.columns[1], 
+                labels={model_year_counts.columns[0]: 'Año del Modelo', 
+                        model_year_counts.columns[1]: 'Cantidad'},
+                title='Cantidad de Vehículos por Año de Modelo')
+
+fig.show()
+if st.checkbox("Construir un diagrama de caja sobre el precio vs condición del vehículo"):
+    fig = px.box(vehicles, x='condition', y='price', title='Precio vs Condición del Vehículo')
+    fig.show()
+if st.checkbox("Construir un gráfico de dispersión sobre la relación entre odómetro y precio"):
+    fig = px.scatter(vehicles, x='odometer', y='price', 
+                 title='Relación entre Odómetro y Precio', 
+                 labels={'odometer': 'Kilometraje', 'price': 'Precio'})
+    fig.show()
+
+    
+if st.checkbox("Crear un diagrama de barras sobre la distribución de tipos de combustible"):
+    fuel_counts = vehicles['fuel'].value_counts().reset_index()
+
+    fig = px.bar(fuel_counts, x=fuel_counts.columns[0], y=fuel_counts.columns[1], 
+                labels={fuel_counts.columns[0]: 'Tipo de Combustible', 
+                        fuel_counts.columns[1]: 'Cantidad'},
+                title='Distribución de Tipos de Combustible')
+
+    fig.show()
