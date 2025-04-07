a9cc3b03d5cd168583b1876b53d710ba9daf06ab
Emmanuel <emmanuelmaldonado2101@gmail.com>
2025-04-07 00:03:53 -0600

    Se cambio el método con el cual se mostraban las gráficas para que se mostraran sin necesidad de abrir nuevas pestañas

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ vehicles = pd.read_csv('vehicles_us.csv') # leer los datos
 st.header('Analisis de un dataframe sobre coches')
 if st.checkbox('Construir histograma para visualizar la distribución de precios'):
     fig = px.histogram(vehicles, x='price', nbins=50, title='Distribución de Precios')
-    fig.show()
+    st.plotly_chart(fig) # Para actualizar correctamente cuando se marque la casilla
+
 
 if st.checkbox("Construir un gráfico de barras de la cantidad de vehículos por año de modelo"):
     model_year_counts = vehicles['model_year'].value_counts().reset_index()
@@ -15,16 +16,17 @@ if st.checkbox("Construir un gráfico de barras de la cantidad de vehículos por
                 labels={model_year_counts.columns[0]: 'Año del Modelo', 
                         model_year_counts.columns[1]: 'Cantidad'},
                 title='Cantidad de Vehículos por Año de Modelo')
+    st.plotly_chart(fig)
 
-    fig.show()
 if st.checkbox("Construir un diagrama de caja sobre el precio vs condición del vehículo"):
     fig = px.box(vehicles, x='condition', y='price', title='Precio vs Condición del Vehículo')
-    fig.show()
+    st.plotly_chart(fig)
+
 if st.checkbox("Construir un gráfico de dispersión sobre la relación entre odómetro y precio"):
     fig = px.scatter(vehicles, x='odometer', y='price', 
                  title='Relación entre Odómetro y Precio', 
                  labels={'odometer': 'Kilometraje', 'price': 'Precio'})
-    fig.show()
+    st.plotly_chart(fig)
 
     
 if st.checkbox("Crear un diagrama de barras sobre la distribución de tipos de combustible"):
@@ -35,4 +37,4 @@ if st.checkbox("Crear un diagrama de barras sobre la distribución de tipos de c
                         fuel_counts.columns[1]: 'Cantidad'},
                 title='Distribución de Tipos de Combustible')
 
-    fig.show()
+    st.plotly_chart(fig)
